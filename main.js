ddf36b9b3b3f84d5643b2e67e857ec092d2cde81
wils200823 <115590910+wils200823@users.noreply.github.com>
2022-10-31 17:44:25 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+window.addEventListener('scroll', () => {
+    document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 0)
+})
+
+
+
+// Show/Hide FAQ answers For each
+const faqs = document.querySelectorAll('.faq');
+
+faqs.forEach(faq => {
+    faq.addEventListener('click', () => {
+        faq.classList.toggle('open');
+
+        const icon = document.querySelector('.faq-icon i')
+        if(icon.className === 'uil uil-plus') {
+            icon.className = "uil uil-minus";
+        } else{
+            icon.className = "uil uil-plus";
+        }
+    })
+})
+
+
+
+// Show/Hide NavMenu on media Queries
+
+const menu = document.querySelector(".navMenu");
+const menuBtn = document.querySelector("#open-menu-btn");
+const closeBtn = document.querySelector("#close-menu-btn");
+
+
+menuBtn.addEventListener('click', () => {
+    menu.style.display = "flex";
+    closeBtn.style.display = "inline-block";
+    menuBtn.style.display = "none";
+})
+
+const closeNav = () => {
+    menu.style.display = "none";
+    closeBtn.style.display = "none";
+    menuBtn.style.display = "inline-block";
+}
+
+
+closeBtn.addEventListener('click', closeNav)
